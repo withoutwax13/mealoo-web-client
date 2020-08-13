3fb262d2a739d20980a1a63b1627a456a4e7afd0
JP Valera <johnpatrickyusoresvalera@gmail.com>
2020-08-13 16:58:26 +0800

    Added Mealoo Logo for Footer component; Modified Footer component styling and content

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,10 +1,37 @@
 import React from 'react'
 
+import FooterLogo from '../../assets/footer-logo.svg'
+
+import { 
+		FooterContainer, 
+		TextFooterSectionContainer,
+		NonTextSectionContainer,
+		LogoContainer,
+		FooterNavItemsContainer,
+		CopyrightTextContainer,
+		NavLink 
+	} from './styles'
 
 export default () => {
 	return (
-		<div>
-			Footer
-		</div>
+		<FooterContainer>
+			<TextFooterSectionContainer>
+				<FooterNavItemsContainer>
+					<NavLink to='/help'>Help</NavLink>
+					<NavLink to='/about'>About</NavLink>
+					<NavLink to='/contact-us'>Contact</NavLink>
+				</FooterNavItemsContainer>
+				<CopyrightTextContainer>
+					<p id='copyright'>
+						Copyright © 2020 Mealoo Code Team
+					</p>
+				</CopyrightTextContainer>
+			</TextFooterSectionContainer>
+			<NonTextSectionContainer>
+				<LogoContainer>
+					<FooterLogo/>
+				</LogoContainer>
+			</NonTextSectionContainer>
+		</FooterContainer>
 	)
 }
---
+++ src/components/Footer/styles.js
@@ -0,0 +1,135 @@
+import styled from 'styled-components'
+import { Link } from 'react-router-dom'
+
+export const FooterContainer = styled.div`
+	margin: 0;
+	background-color: #325d79ff;
+	padding-top: 25px; padding-bottom: 25px;
+	@media (min-width: 1281px) {
+		display: flex;
+		flex-direction: row;
+		justify-content: center;
+	}
+
+	@media (min-width: 1025px) and (max-width: 1280px) {
+		display: flex;
+		flex-direction: row;
+		justify-content: center;
+	}
+
+	@media (min-width: 768px) and (max-width: 1024px) {
+		display: flex;
+		flex-direction: row;
+		justify-content: center;
+	}
+
+	@media (min-width: 768px) and (max-width: 1024px) and (orientation: landscape) {
+		display: flex;
+		flex-direction: row;
+		justify-content: center;
+	}
+
+	@media (min-width: 481px) and (max-width: 767px) {
+		display: flex;
+		flex-direction: column-reverse;
+		align-items: center;
+	}
+
+	@media (min-width: 320px) and (max-width: 480px) {
+		display: flex;
+		flex-direction: column-reverse;
+		align-items: center;
+	}
+`
+
+export const TextFooterSectionContainer = styled.div`
+	display: flex;
+	flex-direction: column;
+	align-items: center;
+`
+
+export const NonTextSectionContainer = styled.div`
+	margin: auto 100px;
+`
+
+export const LogoContainer = styled.div`
+	@media (min-width: 481px) and (max-width: 767px) {
+		margin-bottom: 10px;
+	}
+
+	@media (min-width: 320px) and (max-width: 480px) {
+		margin-bottom: 10px;
+	}
+`
+
+export const FooterNavItemsContainer = styled.div`
+	@media (min-width: 1281px) {
+		display: flex;
+		flex-direction: row;
+		margin-bottom: 5px;
+	}
+
+	@media (min-width: 1025px) and (max-width: 1280px) {
+		display: flex;
+		flex-direction: row;
+		margin-bottom: 5px;
+	}
+
+	@media (min-width: 768px) and (max-width: 1024px) {
+		display: flex;
+		flex-direction: row;
+		margin-bottom: 5px;
+	}
+
+	@media (min-width: 768px) and (max-width: 1024px) and (orientation: landscape) {
+		display: flex;
+		flex-direction: row;
+		margin-bottom: 5px;
+	}
+
+	@media (min-width: 481px) and (max-width: 767px) {
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		margin-bottom: 10px;
+	}
+
+	@media (min-width: 320px) and (max-width: 480px) {
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		margin-bottom: 10px;
+	}
+`
+
+export const CopyrightTextContainer = styled.div`
+	color: white;
+`
+
+export const NavLink = styled(Link)`
+	color: white;
+	font-size: 16px;
+	@media (min-width: 1281px) {
+		margin: 0 10px 5px 10px;
+	}
+
+	@media (min-width: 1025px) and (max-width: 1280px) {
+		margin: 0 10px 5px 10px;
+	}
+
+	@media (min-width: 768px) and (max-width: 1024px) {
+		margin: 0 10px 5px 10px;
+	}
+
+	@media (min-width: 768px) and (max-width: 1024px) and (orientation: landscape) {
+		margin: 0 10px 5px 10px;
+	}
+
+	@media (min-width: 481px) and (max-width: 767px) {
+		margin: 3px 0 3px 0;	
+	}
+
+	@media (min-width: 320px) and (max-width: 480px) {
+		margin: 3px 0 3px 0;	
+	}
+`
