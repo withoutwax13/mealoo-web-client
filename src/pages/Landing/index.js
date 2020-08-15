489648b40194b76d37f9431e3d5e8d82ec979349
JP Valera <johnpatrickyusoresvalera@gmail.com>
2020-08-15 15:43:33 +0800

    Modified styling and layout of Hero Section in Landing page

--- src/pages/Landing/index.js
+++ src/pages/Landing/index.js
@@ -1,18 +1,61 @@
 import React from 'react'
 
+import AuthButton from '../../components/Button/Auth/index'
+
 import {
 		LandingPageContainer, 
 		HeroSectionContainer, 
 		FeatureCollectionContainer, 
 		FeatureItemContainer,
-		ProductTrialContainer
+		ProductTrialContainer,
+		HeroSectionMainContent,
+		HeroSectionMainHeading,
+		HeroSectionMainHeadingContainer,
+		HeroSectionSubHeading,
+		HeroSectionSubHeadingContainer,
+		HeroSectionActionButtonContainer,
+		HeroSectionImageStyle,
+		ActionButtonMobileContainer,
+		ActionButtonTabletContainer,
+		ActionButtonDesktopContainer
 	} from './styles'
 
 export default () => {
 	return (
 		<LandingPageContainer>
 			<HeroSectionContainer>
-				HeroSectionContainer
+				<HeroSectionMainContent>
+					<HeroSectionMainHeadingContainer>
+						<HeroSectionMainHeading>
+							just eat
+						</HeroSectionMainHeading>
+					</HeroSectionMainHeadingContainer>
+					<HeroSectionSubHeadingContainer>
+						<HeroSectionSubHeading>
+							WE'LL HANDLE THE REST
+						</HeroSectionSubHeading>
+					</HeroSectionSubHeadingContainer>
+					<HeroSectionActionButtonContainer>
+						<ActionButtonMobileContainer>
+							<AuthButton width={82.955} height={30.682}>
+								<h4>JOIN</h4>
+							</AuthButton>
+						</ActionButtonMobileContainer>
+						<ActionButtonTabletContainer>
+							<AuthButton width={112.223} height={43.690}>
+								<h2>JOIN</h2>
+							</AuthButton>
+						</ActionButtonTabletContainer>
+						<ActionButtonDesktopContainer>
+							<AuthButton width={146.159} height={54.479}>
+								<h1>JOIN</h1>
+							</AuthButton>
+						</ActionButtonDesktopContainer>
+					</HeroSectionActionButtonContainer>
+				</HeroSectionMainContent>
+				<HeroSectionImageStyle>
+					SVG IMAGE ASSET HERE
+				</HeroSectionImageStyle>
 			</HeroSectionContainer>
 			<FeatureCollectionContainer>
 				<FeatureItemContainer>
--- src/pages/Landing/styles.js
+++ src/pages/Landing/styles.js
@@ -7,6 +7,9 @@ export const LandingPageContainer = styled.main`
 export const HeroSectionContainer = styled.div`
 	height: 500px;
 	width: 100%;
+	display: flex;
+	flex-direction: row;
+	justify-content: space-between;
 `
 
 export const FeatureCollectionContainer = styled.div`
@@ -21,4 +24,214 @@ export const FeatureItemContainer = styled.div`
 export const ProductTrialContainer = styled.div`
 	height: 500px;
 	width: 100%;
+`
+
+export const HeroSectionMainContent = styled.div`
+	display: flex;
+	flex-direction: column;
+	margin-top: 40px;
+	margin-left: 10%;
+`
+
+export const HeroSectionMainHeading = styled.h1`
+	color: #f26627ff;
+	font-family: sans serif;
+	@media (min-width: 1281px) {
+		font-weight: bold;
+		font-size: 85.33px;
+	}
+
+	@media (min-width: 1025px) and (max-width: 1280px) {
+		font-weight: bold;
+		font-size: 85.33px;
+	}
+
+	@media (min-width: 768px) and (max-width: 1024px) {
+		font-weight: bold;
+		font-size: 64px;
+	}
+
+	@media (min-width: 768px) and (max-width: 1024px) and (orientation: landscape) {
+		font-weight: bold;
+		font-size: 64px;
+	}
+
+	@media (min-width: 481px) and (max-width: 767px) {
+		font-weight: bold;
+		font-size: 37.33px;
+	}
+
+	@media (min-width: 320px) and (max-width: 480px) {
+		font-weight: bold;
+		font-size: 37.33px;
+	}
+`
+export const HeroSectionMainHeadingContainer = styled.div`
+	margin: 5px auto 5px 0;
+`
+
+export const HeroSectionSubHeading = styled.h3`
+	color: #f9a26cff;
+	font-family: sans serif;
+	@media (min-width: 1281px) {
+		font-weight: bold;
+		font-size: 32px;
+	}
+
+	@media (min-width: 1025px) and (max-width: 1280px) {
+		font-weight: bold;
+		font-size: 32px;
+	}
+
+	@media (min-width: 768px) and (max-width: 1024px) {
+		font-weight: bold;
+		font-size: 24px;
+	}
+
+	@media (min-width: 768px) and (max-width: 1024px) and (orientation: landscape) {
+		font-weight: bold;
+		font-size: 24px;
+	}
+
+	@media (min-width: 481px) and (max-width: 767px) {
+		font-weight: bold;
+		font-size: 17.33px;
+	}
+
+	@media (min-width: 320px) and (max-width: 480px) {
+		font-weight: bold;
+		font-size: 17.33px;
+	}
+`
+export const HeroSectionSubHeadingContainer = styled.div`
+	margin: 5px auto 5px 0;
+	@media (min-width: 1281px) {
+		overflow-wrap: break-word;
+		width: 266.319px;
+		height: 64.734px;
+	}
+
+	@media (min-width: 1025px) and (max-width: 1280px) {
+		overflow-wrap: break-word;
+		width: 266.319px;
+		height: 64.734px;
+	}
+
+	@media (min-width: 768px) and (max-width: 1024px) {
+		overflow-wrap: break-word;
+		width: 200.014px;
+		height: 48.551px;
+	}
+
+	@media (min-width: 768px) and (max-width: 1024px) and (orientation: landscape) {
+		overflow-wrap: break-word;
+		width: 200.014px;
+		height: 48.551px;
+	}
+
+	@media (min-width: 481px) and (max-width: 767px) {
+		overflow-wrap: break-word;
+		width: 144.76px;
+		height: 35.064px;
+	}
+
+	@media (min-width: 320px) and (max-width: 480px) {
+		overflow-wrap: break-word;
+		width: 144.76px;
+		height: 35.064px;
+	}
+`
+
+export const HeroSectionActionButtonContainer = styled.div`
+	margin: 30px auto 5px 0;
+`
+
+export const HeroSectionImageStyle = styled.div`
+	margin: 80px 0 auto auto;
+	border-top: 1px solid silver;
+	border-left: 1px solid silver;
+	border-bottom: 1px solid silver;
+	border-top-left-radius: 20px;
+	border-bottom-left-radius: 20px;
+	box-shadow: gray 0 0 5px 0;
+	@media (min-width: 1281px) {
+		width: 600px;
+		height: 300px;
+	}
+
+	@media (min-width: 1025px) and (max-width: 1280px) {
+		width: 600px;
+		height: 300px;
+	}
+
+	@media (min-width: 768px) and (max-width: 1024px) {
+		width: 400px;
+		height: 300px;
+	}
+
+	@media (min-width: 768px) and (max-width: 1024px) and (orientation: landscape) {
+		width: 400px;
+		height: 300px;	
+	}
+
+	@media (min-width: 481px) and (max-width: 767px) {
+		width: 350px;
+		height: 300px;
+	}
+
+	@media (min-width: 320px) and (max-width: 480px) {
+		width: 350px;
+		height: 300px;	
+	}
+`
+
+export const ActionButtonMobileContainer = styled.div`
+	@media (min-width: 1281px) {
+		display: none;
+	}
+
+	@media (min-width: 1025px) and (max-width: 1280px) {
+		display: none;
+	}
+
+	@media (min-width: 768px) and (max-width: 1024px) {
+		display: none;
+	}
+
+	@media (min-width: 768px) and (max-width: 1024px) and (orientation: landscape) {
+		display: none;
+	}
+`
+
+export const ActionButtonTabletContainer = styled.div`
+	@media (min-width: 1281px) {
+		display: none;
+	}
+
+	@media (min-width: 1025px) and (max-width: 1280px) {
+		display: none;
+	}
+	@media (min-width: 481px) and (max-width: 767px) {
+		display: none;
+	}
+
+	@media (min-width: 320px) and (max-width: 480px) {
+		display: none;
+	}
+`
+export const ActionButtonDesktopContainer = styled.div`
+	@media (min-width: 768px) and (max-width: 1024px) {
+		display: none;
+	}
+
+	@media (min-width: 768px) and (max-width: 1024px) and (orientation: landscape) {
+		display: none;
+	}
+	@media (min-width: 481px) and (max-width: 767px) {
+		display: none;
+	}
+
+	@media (min-width: 320px) and (max-width: 480px) {
+		display: none;
+	}
 `
