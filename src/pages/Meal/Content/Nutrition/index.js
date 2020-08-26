7baf5ee93bb62120d06f59a53bd1f3dbc2a92689
JP Valera <johnpatrickyusoresvalera@gmail.com>
2020-08-26 23:24:25 +0800

    Updated Nutrition Component

--- src/pages/Meal/Content/Nutrition/index.js
+++ src/pages/Meal/Content/Nutrition/index.js
@@ -1,15 +1,60 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 
-import { Container } from './style'
+import { 
+        Container, 
+        SectionTitle,
+        NutritionItem, 
+        Label, 
+        NutritionAmount 
+    } from './style'
 
 const Nutrition = (props) => {
 
-    const { calories, macronutrients } = props
+    const { 
+        calories = 0, 
+        macronutrients = { 
+            carbohydrates: 0,
+            proteins: 0,
+            fats: 0  
+        } 
+    } = props
+    const { carbohydrates, proteins, fats } = macronutrients
 
     return (
         <Container>
-            Nutrition
+            <SectionTitle>Nutritional Detail</SectionTitle>
+            <div 
+                style={{
+                    display: 'flex', 
+                    flexDirection: 'row', 
+                    justifyContent: 'flex-end',
+                    marginTop: '10px',
+                    marginBottom: '5px'
+                }}>
+                <Label color={`orange`}>Per 100g</Label>
+            </div>
+            <div style={{
+                    marginTop: '10px', 
+                    marginBottom: '10px'
+                }}>
+                <NutritionItem>
+                    <Label color='#ff4949ff'>Calories</Label>
+                    <NutritionAmount>{calories}</NutritionAmount>
+                </NutritionItem>
+                <NutritionItem>
+                    <Label color='#bb77bbff'>Carbohydrates</Label>
+                    <NutritionAmount>{carbohydrates}</NutritionAmount>
+                </NutritionItem>
+                <NutritionItem>
+                    <Label color={`#c9b35cff`}>Proteins</Label>
+                    <NutritionAmount>{proteins}</NutritionAmount>
+                </NutritionItem>
+                <NutritionItem>
+                    <Label color={`#e39999ff`}>Fats</Label>
+                    <NutritionAmount>{fats}</NutritionAmount>
+                </NutritionItem>
+            </div>
         </Container>
     )
 }
--- src/pages/Meal/Content/Nutrition/style.js
+++ src/pages/Meal/Content/Nutrition/style.js
@@ -1,5 +1,39 @@
 import styled from 'styled-components'
 
 export const Container = styled.div`
+    border: 1px solid silver;
+    border-radius: 10px;
+    width: 300px;
+    display: flex;
+    flex-direction: column;
+    padding: 15px;
+`
 
+export const SectionTitle = styled.div`
+    margin: 0; padding: 0;
+    font-size: 20px;
+    color: black;
+    font-family: Monospace;
+    font-weight: bolder;
+    margin: 5px auto;
+`
+
+export const NutritionItem = styled.div`
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+    margin: 5px 0;
+`
+
+export const Label = styled.div`
+    font-family: Monospace;
+    font-size: 16px;
+    font-weight: bold;
+    color: ${(props)=>props.color};
+`
+
+export const NutritionAmount = styled.div`
+    font-family: Monospace;
+    font-size: 16px;
+    font-weight: bold;
 `
