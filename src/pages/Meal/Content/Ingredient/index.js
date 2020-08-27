6cee892966a36b8327e19ae1de18814116a79913
JP Valera <johnpatrickyusoresvalera@gmail.com>
2020-08-27 20:10:46 +0800

    Added react-tooltip; Update Ingredient component

--- src/pages/Meal/Content/Ingredient/index.js
+++ src/pages/Meal/Content/Ingredient/index.js
@@ -1,14 +1,84 @@
-import React from 'react'
+import React, { Fragment } from 'react'
+import ReactTooltip from 'react-tooltip'
 import PropTypes from 'prop-types'
 
-import { Container } from './style'
+import { 
+        Container, 
+        ProductItem, 
+        ProductLabel, 
+        ProductTooltip, 
+        ProductsContainer, 
+        SectionTitle 
+    } from './style'
 
 const Ingredient = (props) => {
 
-    const { ingredientData } = props
+    const {
+            ingredientData = [
+                {
+                    product: {
+                        name: 'Egg', price: 22, caloriesPer100g: 10
+                    },
+                    amount: 2
+                },
+                {
+                    product: {
+                        name: 'Cabbage', price: 76, caloriesPer100g: 5
+                    },
+                    amount: 6
+                },
+                {
+                    product: {
+                        name: 'Beef', price: 12, caloriesPer100g: 189
+                    },
+                    amount: 4
+                }
+            ]
+        } = props
+
+    const renderIngredients = () => {
+            return (
+                <ProductsContainer>
+                    {ingredientData.map(data=>{
+                        return (
+                            <ProductItem key={`${data.amount}-${data.product.name}`}>
+                                <ProductLabel 
+                                    data-tip 
+                                    data-for={`${data.amount}-${data.product.name}`}
+                                    >
+                                        {data.product.name}
+                                </ProductLabel>
+                                <ReactTooltip 
+                                    id={`${data.amount}-${data.product.name}`}
+                                    type='info'>
+                                        <ProductTooltip>
+                                            <h4>Product Details</h4>
+                                            <p>
+                                                <strong>Amount:  </strong> 
+                                                {data.amount}
+                                            </p>
+                                            <p>
+                                                <strong>Price:  </strong> 
+                                                {data.product.price}
+                                            </p>
+                                            <p>
+                                                <strong>Calories per 100g:  </strong> 
+                                                {data.product.caloriesPer100g}
+                                            </p>
+                                        </ProductTooltip>
+                                </ReactTooltip>
+                            </ProductItem>
+                        )
+                    })}
+                </ProductsContainer>
+            )
+    }
 
     return (
-        <Container>Ingredient</Container>
+        <Container>
+            <SectionTitle>Meal Ingredients:</SectionTitle>
+            {renderIngredients()}
+        </Container>
     )
 }
 
--- src/pages/Meal/Content/Ingredient/style.js
+++ src/pages/Meal/Content/Ingredient/style.js
@@ -1,5 +1,73 @@
 import styled from 'styled-components'
 
 export const Container = styled.div`
+    border: 1px solid silver;
+    border-radius: 10px;
+    padding: 15px;
+    @media (min-width: 1281px) {
+        display: flex;
+        flex-direction: row;
+        width: 60vw;
+	}
 
+	@media (min-width: 1025px) and (max-width: 1280px) {
+        display: flex;
+        flex-direction: row;
+        width: 60vw;
+	}
+
+	@media (min-width: 768px) and (max-width: 1024px) {
+        display: flex;
+        flex-direction: row;
+        width: 60vw;
+	}
+
+	@media (min-width: 768px) and (max-width: 1024px) and (orientation: landscape) {
+        display: flex;
+        flex-direction: row;
+        width: 60vw;
+	}
+
+	@media (min-width: 481px) and (max-width: 767px) {
+        display: flex;
+        flex-direction: column;
+        width: 90vw;
+	}
+
+	@media (min-width: 320px) and (max-width: 480px) {
+        display: flex;
+        flex-direction: column;
+        width: 90vw;
+    }
+`
+export const SectionTitle = styled.div`
+    color: black;
+    font-size: 20px;
+    font-family: Courier New;
+    font-weight: bold;
+    margin: 5px;
+`
+export const ProductsContainer = styled.div`
+    display: flex;
+    flex-direction: row;
+    justify-content: center;
+    overflow-x: auto;
+`
+export const ProductItem = styled.div`
+    border-radius: 10px;
+    border: 1px solid silver;
+    padding: 5px;
+    background-color: pink;
+    color: gray;
+    height: 30px;
+    margin: 0 2px;
+`
+export const ProductLabel = styled.div`
+    font-family: Monospace;
+    font-size: 16px;
+`
+export const ProductTooltip = styled.div`
+    display: flex;
+    flex-direction: column;
+    width: 150px;
 `
