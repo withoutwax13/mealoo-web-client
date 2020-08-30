40141bc356f92f94e007fe4227ef078c05735e7e
JP Valera <johnpatrickyusoresvalera@gmail.com>
2020-08-30 23:49:51 +0800

    Update Main component

--- src/pages/PersonalizedMeals/Main/index.js
+++ src/pages/PersonalizedMeals/Main/index.js
@@ -1,15 +1,31 @@
 import React from 'react'
+import PropTypes from 'prop-types'
+
+import Meal from './Meal/index'
 
 import {
     Container
 } from './style'
 
-const Main = () => {
+const Main = (props) => {
+
+    const {
+        setData = [
+            { name: 'Meal 1', image: null},
+            { name: 'Meal 2', image: null},
+            { name: 'Meal 3', image: null}
+        ]
+    } = props
+
     return (
         <Container>
-            Main
+            {setData.map((meal, index)=><Meal key={index} mealData={meal}/>)}
         </Container>
     )
+}
+
+Main.propTypes = {
+    setData: PropTypes.array
 }
 
 export default Main
