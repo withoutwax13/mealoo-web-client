5a48fc14df2ea54833cd83a6fe450363fa63598e
JP Valera <johnpatrickyusoresvalera@gmail.com>
2020-09-04 00:25:49 +0800

    Update axios instance baseURL to accomodate the TEMPORARY solution to CORS error

--- src/utils/API.js
+++ src/utils/API.js
@@ -1,7 +1,9 @@
 import axios from 'axios'
 
+{/* Note: adding https://cors-anywhere.herokuapp.com as CORS error solution is only temporary */}
+
 const API = axios.create({
-	baseURL: 'https://mealoodietapp.herokuapp.com/'
+	baseURL: 'https://cors-anywhere.herokuapp.com/https://mealoodietapp.herokuapp.com/'
 })
 
 export default API
