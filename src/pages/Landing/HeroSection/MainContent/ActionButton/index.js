777fd1e71514ec8eae7917c472abfa2a37b3fe9e
JP Valera <johnpatrickyusoresvalera@gmail.com>
2020-09-07 00:24:00 +0800

    Update Auth ActionButton component in Here Section of Landing page

--- src/pages/Landing/HeroSection/MainContent/ActionButton/index.js
+++ src/pages/Landing/HeroSection/MainContent/ActionButton/index.js
@@ -1,6 +1,10 @@
 import React from 'react'
+import PropTypes from 'prop-types'
+import { connect } from 'react-redux'
 
 import AuthButton from '../../../../../components/Button/Auth/index'
+
+import { clientLogIn, clientLogOut } from '../../../../../modules/actions'
 
 import {
 			HeroSectionActionButtonContainer,
@@ -9,24 +13,51 @@ import {
 			ActionButtonDesktopContainer,
 } from './style'
 
-export default () => {
+const ActionButton = (props) => {
+
+	const { isClientHasAuth, clientLogOut, clientLogIn } = props
+
+	const clickCallback = () => {
+		return isClientHasAuth ? clientLogOut : clientLogIn
+	}
+
 	return (
 		<HeroSectionActionButtonContainer>
 			<ActionButtonMobileContainer>
-				<AuthButton width={82.955} height={30.682}>
-					<h4>JOIN</h4>
-				</AuthButton>
+				<AuthButton 
+					width={82.955} 
+					height={30.682} 
+					clickCallback={clickCallback()}
+					childrenNode={<h4>{isClientHasAuth ? `LOGOUT` : `JOIN`}</h4>}/>
 			</ActionButtonMobileContainer>
 			<ActionButtonTabletContainer>
-				<AuthButton width={112.223} height={43.690}>
-					<h2>JOIN</h2>
-				</AuthButton>
+				<AuthButton 
+					width={112.223} 
+					height={43.690}
+					clickCallback={clickCallback()}
+					childrenNode={<h2>{isClientHasAuth ? `LOGOUT` : `JOIN`}</h2>}/>
 			</ActionButtonTabletContainer>
 			<ActionButtonDesktopContainer>
-				<AuthButton width={146.159} height={54.479}>
-					<h1>JOIN</h1>
-				</AuthButton>
+				<AuthButton 
+					width={146.159} 
+					height={54.479}
+					clickCallback={clickCallback()}
+					childrenNode={<h1>{isClientHasAuth ? `LOGOUT` : `JOIN`}</h1>}/>
 			</ActionButtonDesktopContainer>
 		</HeroSectionActionButtonContainer>
 	)
 }
+
+ActionButton.propTypes = {
+	isClientHasAuth: PropTypes.bool,
+	clientLogIn: PropTypes.func,
+	clientLogOut: PropTypes.func
+}
+
+const mapStateToProps = ({isClientHasAuth}) => {
+	return {
+		isClientHasAuth
+	}
+}
+
+export default connect(mapStateToProps, { clientLogIn, clientLogOut })(ActionButton)
