eabff4cb0c11f88a6957760a93f8fdb9e01a76c8
JP Valera <johnpatrickyusoresvalera@gmail.com>
2020-09-09 20:37:17 +0800

    Update Profile button of PersonalizedMeals page to redirect /user route

--- src/pages/PersonalizedMeals/Head/Profile/index.js
+++ src/pages/PersonalizedMeals/Head/Profile/index.js
@@ -1,16 +1,19 @@
 import React from 'react'
 import styled from 'styled-components'
 
+import History from '../../../../utils/History'
+
 import ProfileImage from '../../../../assets/Meal/profile.svg'
 
 export default () => {
     return (
         <Container>
-            <ProfileImage/>
+            <ProfileImage onClick={()=>History.push('/user')}/>
         </Container>
     )
 }
 
 const Container = styled.div`
     margin: 0 30px 0 auto;
+    cursor: pointer;
 `
