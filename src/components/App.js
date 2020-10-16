4b02448b2f3e3bf83a5e4a09d0bbae8b24d94238
John Patrick Valera <johnpatrickyusoresvalera@gmail.com>
2020-10-17 01:21:48 +0800

    Add searching for previous log token mechanism for auto-login feature

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,20 @@
-import React from 'react'
+import React, { useEffect } from "react";
+import PropTypes from "prop-types";
 
-import Routes from '../utils/Routes'
+import Routes from "../utils/Routes";
+import { connect } from "react-redux";
 
-export default () => {
-	return (
-		<Routes/>
-	)
-}
+import { checkIfClientHasToken } from "../modules/actions";
+
+const App = ({ checkIfClientHasToken }) => {
+  useEffect(() => {
+    checkIfClientHasToken();
+  }, []);
+  return <Routes />;
+};
+
+App.propTypes = {
+  checkIfClientHasToken: PropTypes.func.isRequired,
+};
+
+export default connect(null, { checkIfClientHasToken })(App);
--- src/utils/Routes.js
+++ src/utils/Routes.js
@@ -1,43 +1,57 @@
-import React from 'react'
-import { Router, Switch, Route } from 'react-router-dom'
+import React from "react";
+import PropTypes from "prop-types";
+import { Router, Switch, Route } from "react-router-dom";
+import { connect } from "react-redux";
 
-import History from './History'
+import History from "./History";
 
-import Landing from '../pages/Landing/index'
-import Meal from '../pages/Meal/index'
-import PersonalizedMeals from '../pages/PersonalizedMeals/index'
-import Profile from '../pages/Profile/index'
-import About from '../pages/About/index'
-import Contact from '../pages/Contact/index'
-import Help from '../pages/Help/index'
-import Login from '../pages/Login/index'
-import Signup from '../pages/Signup/index'
+import Landing from "../pages/Landing/index";
+import Meal from "../pages/Meal/index";
+import PersonalizedMeals from "../pages/PersonalizedMeals/index";
+import Profile from "../pages/Profile/index";
+import About from "../pages/About/index";
+import Contact from "../pages/Contact/index";
+import Help from "../pages/Help/index";
+import Login from "../pages/Login/index";
+import Signup from "../pages/Signup/index";
 
-import Integrated from '../components/Integrated/index'
+import Integrated from "../components/Integrated/index";
 
 // for the mean time the / path directs to Landing page. It will be changed in the future to cater
 // users based on their authorization
 
-export default () => {
-	return (
-		<div>
-			<Router history={History}>
-				<Integrated>
-					<Switch>
-						<Route exact path='/' component={Landing}/>
-						<Route exact path='/landing' component={Landing}/>
-						<Route exact path='/set' component={PersonalizedMeals}/>
-						<Route exact path='/user' component={Profile}/>
-						<Route exact path='/meal' component={Meal}/>
-						<Route exact path='/about' component={About}/>
-						<Route exact path='/help' component={Help}/>
-						<Route exact path='/contact-us' component={Contact}/>
-						<Route exact path='/login' component={Login}/>
-						<Route exact path='/signup' component={Signup}/>
-						<Route component={Landing}/>
-					</Switch>
-				</Integrated>
-			</Router>
-		</div>
-	)
-}
+const Routes = ({ logData }) => {
+  return (
+    <div>
+      <Router history={History}>
+        <Integrated>
+          <Switch>
+            <Route exact path="/" component={logData ? Login : Landing} />
+            <Route exact path="/landing" component={Landing} />
+            <Route exact path="/set" component={PersonalizedMeals} />
+            <Route exact path="/user" component={Profile} />
+            <Route exact path="/meal" component={Meal} />
+            <Route exact path="/about" component={About} />
+            <Route exact path="/help" component={Help} />
+            <Route exact path="/contact-us" component={Contact} />
+            <Route exact path="/login" component={Login} />
+            <Route exact path="/signup" component={Signup} />
+            <Route component={Landing} />
+          </Switch>
+        </Integrated>
+      </Router>
+    </div>
+  );
+};
+
+const mapStateToProps = ({ clientLogDetails }) => {
+  return {
+    logData: clientLogDetails,
+  };
+};
+
+Routes.propTypes = {
+  logData: PropTypes.any,
+};
+
+export default connect(mapStateToProps)(Routes);
